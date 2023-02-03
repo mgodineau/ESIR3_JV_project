2ec49104c31f398a158dba71fa0d8ac641dc5f12
mgodineau <mathieu.godineau.35@gmail.com>
2023-02-03 23:43:47 +0100

    Optimized marching cube

--- Assets/DestructionSystem/Core/VoxBuilderMarchingCube.cs
+++ Assets/DestructionSystem/Core/VoxBuilderMarchingCube.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.Linq;
 using DestructionSystem.Utils;
+using Unity.VisualScripting;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Serialization;
 
 
 namespace DestructionSystem {
@@ -13,8 +17,7 @@ public class VoxBuilderMarchingCube : IVoxBuilder {
 	
 	
 	[Serializable]
-	private struct VertexLayout
-	{
+	private struct VertexLayout {
 		public static readonly VertexAttributeDescriptor[] Layout = {
 			new(VertexAttribute.Position),
 			new(VertexAttribute.Normal),
@@ -25,26 +28,74 @@ public class VoxBuilderMarchingCube : IVoxBuilder {
 		public Vector3 normal;
 		public Vector2 uv;
 	}
+
+
+	[Serializable]
+	private class VertexBuildData {
+
+		public List<int> triangleIds;
+		public int edgeDir;
+		public Vector3Int metaEdgePos;
+		
+		public VertexBuildData( int edgeDir, Vector3Int metaEdgePos ) {
+			triangleIds = new List<int>();
+			this.edgeDir = edgeDir;
+			this.metaEdgePos = metaEdgePos;
+		}
+	}
+
+	[Serializable]
+	private struct MetaMesh {
+		public byte pattern;
+		public List<int> trianglesIds;
+	}
+	
 	
 	[Serializable]
 	private class MonitoredVertexList : Utils.MonitoredList<VertexLayout> {}
 	
+	[Serializable]
+	private class MonitoredIndexList : Utils.MonitoredList<int> {}
+
+	[Serializable]
+	private class Serializable3DMetaMeshArray : Utils.Serializable3DArray<MetaMesh> {
+		
+		[SerializeField] private List<MetaMesh> _data; 
+		protected override List<MetaMesh> Data {
+			get => _data;
+			set => _data = value;
+		}
+		
+		public Serializable3DMetaMeshArray(Vector3Int dim) : this(dim, new MetaMesh(){pattern = 0, trianglesIds = new List<int>()} ) {}
+		public Serializable3DMetaMeshArray(Vector3Int dim, MetaMesh defaultValue) : base(dim, defaultValue) {}
+	}
+	
 	
 	// lists containing the data to build the mesh with
-	[SerializeField] private List<int> triangles;
+	[SerializeReference] private MonitoredIndexList triangles;
 	[SerializeReference] private MonitoredVertexList vertices;
+	[SerializeField] private List<VertexBuildData> verticesToTriangles;
 
 	[SerializeField] public int metaVoxelSize;
-	[SerializeField] private Serializable3DByteArray metaVoxels;
+	[SerializeField] private Serializable3DIntArray metaVoxels;
+	
+	[SerializeField] private Serializable3DMetaMeshArray metaMeshToTriangles;
+
+	[SerializeField] private Serializable3DIntArray[] vertexIndexes;
+	
 	
 	
 	public VoxBuilderMarchingCube( int metaVoxelSize = 4 ) {
 		
-		triangles = new List<int>();
+		triangles = new MonitoredIndexList();
 		vertices = new MonitoredVertexList();
+		verticesToTriangles = new List<VertexBuildData>();
 
 		this.metaVoxelSize = metaVoxelSize;
-		metaVoxels = new Serializable3DByteArray(Vector3Int.zero);
+		metaVoxels = new Serializable3DIntArray(Vector3Int.zero);
+		metaMeshToTriangles = new Serializable3DMetaMeshArray(Vector3Int.zero);
+		
+		vertexIndexes = new Serializable3DIntArray[3];
 	}
 
 	
@@ -80,20 +131,29 @@ public class VoxBuilderMarchingCube : IVoxBuilder {
 			int startIndex = chunkLocationAndSize.Key * vertices.chunkSize;
 			int currentChunkSize = chunkLocationAndSize.Value * vertices.chunkSize;
 			currentChunkSize = Mathf.Min(currentChunkSize, vertices.Count - startIndex);
-			if (currentChunkSize <= 0) { break;
+			if (currentChunkSize <= 0) { 
+				break;
 			}
 			
 			mesh.SetVertexBufferData(vertices, startIndex, startIndex, currentChunkSize, 0, flags);
 		}
 		vertices.ClearUpdatedChunks();
 
-		// int previousIndexCount = (int)mesh.GetIndexCount(0);
+		
+		//TODO update indexes by chunks
 		mesh.SetIndexBufferParams( triangles.Count, IndexFormat.UInt32 );
-		// if ( previousIndexCount < triangles.Count )
-		// {
-		// 	mesh.SetIndexBufferData(triangles, previousIndexCount, previousIndexCount, triangles.Count - previousIndexCount, flags);
-		// }
-		mesh.SetIndexBufferData( triangles, 0, 0, triangles.Count, flags );
+		foreach (KeyValuePair<int, int> chunkLocationAndSize in triangles.GetUpdatedChunks()) {
+			int startIndex = chunkLocationAndSize.Key * triangles.chunkSize;
+			int currentChunkSize = chunkLocationAndSize.Value * triangles.chunkSize;
+			currentChunkSize = Mathf.Min(currentChunkSize, triangles.Count - startIndex);
+			if (currentChunkSize <= 0) { 
+				break;
+			}
+			
+			mesh.SetIndexBufferData(triangles, startIndex, startIndex, currentChunkSize, flags);
+		}
+		triangles.ClearUpdatedChunks();
+		
 		
 		mesh.subMeshCount = 1;
 		mesh.SetSubMesh(0, new SubMeshDescriptor(0, triangles.Count), flags);
@@ -108,8 +168,15 @@ public class VoxBuilderMarchingCube : IVoxBuilder {
 		
 		Vector3 modelSize = model.BoundingBox;
 		Vector3Int metaVoxelBoundingBox = Vector3Int.CeilToInt(modelSize / metaVoxelSize);
-		metaVoxels = new Serializable3DByteArray(metaVoxelBoundingBox);
-
+		metaVoxels = new Serializable3DIntArray(metaVoxelBoundingBox);
+		metaMeshToTriangles = new Serializable3DMetaMeshArray( metaVoxelBoundingBox + Vector3Int.one );
+		verticesToTriangles = new List<VertexBuildData>();
+		
+		vertexIndexes = new Serializable3DIntArray[3]{ 
+			new (metaVoxelBoundingBox+Vector3Int.one*2, -1),
+			new (metaVoxelBoundingBox+Vector3Int.one*2, -1),
+			new (metaVoxelBoundingBox+Vector3Int.one*2, -1)
+		};
 		
 		
 		// build the array of meta voxels
@@ -124,16 +191,16 @@ public class VoxBuilderMarchingCube : IVoxBuilder {
 				}
 			}
 		}
-
-		vertices = new MonitoredVertexList();
+		
+		vertices.Clear();
 		triangles.Clear();
 		
 		
 		// build every meta mesh
 		Vector3Int metaMeshPos = Vector3Int.zero;
-		for (metaMeshPos.x = -1; metaMeshPos.x < metaVoxels.Dim.x; metaMeshPos.x++) {
-			for (metaMeshPos.y = -1; metaMeshPos.y < metaVoxels.Dim.y; metaMeshPos.y++) {
-				for (metaMeshPos.z = -1; metaMeshPos.z < metaVoxels.Dim.z; metaMeshPos.z++) {
+		for (metaMeshPos.x = 0; metaMeshPos.x < metaMeshToTriangles.Dim.x; metaMeshPos.x++) {
+			for (metaMeshPos.y = 0; metaMeshPos.y < metaMeshToTriangles.Dim.y; metaMeshPos.y++) {
+				for (metaMeshPos.z = 0; metaMeshPos.z < metaMeshToTriangles.Dim.z; metaMeshPos.z++) {
 					RefreshMetaMesh(model, metaMeshPos);
 				}
 			}
@@ -142,54 +209,233 @@ public class VoxBuilderMarchingCube : IVoxBuilder {
 	}
 	
 	public void RefreshRegion(IVoxModel model, Vector3 cornerLow, Vector3 cornerHigh) {
-		RefreshEntireModel(model);
-		// throw new System.NotImplementedException();
+
+		Vector3Int voxelCornerLow = model.ObjectToVoxelPosition(cornerLow);
+		Vector3Int voxelCornerHigh = model.ObjectToVoxelPosition(cornerHigh);
+		
+		Vector3Int metaCornerLow = Vector3Int.FloorToInt( voxelCornerLow / metaVoxelSize );
+		Vector3Int metaCornerHigh = Vector3Int.CeilToInt( voxelCornerHigh / metaVoxelSize );
+		metaCornerLow = Vector3Int.Max(metaCornerLow, Vector3Int.zero);
+		metaCornerHigh = Vector3Int.Min( metaCornerHigh, metaVoxels.Dim-Vector3Int.one );
+		
+		// refersh metaVoxels
+		Vector3Int metaVoxelPos = Vector3Int.zero;
+		for ( metaVoxelPos.x = metaCornerLow.x; metaVoxelPos.x <= metaCornerHigh.x; metaVoxelPos.x++ ) {
+			for (metaVoxelPos.y = metaCornerLow.y; metaVoxelPos.y <= metaCornerHigh.y; metaVoxelPos.y++) {
+				for (metaVoxelPos.z = metaCornerLow.z; metaVoxelPos.z <= metaCornerHigh.z; metaVoxelPos.z++) {
+					metaVoxels[metaVoxelPos] = CountVoxels(model, metaVoxelPos);
+				}
+			}
+		}
+		
+		
+		
+		// build every meta mesh
+		Vector3Int metaMeshPos = Vector3Int.zero;
+		for (metaMeshPos.x = metaCornerLow.x; metaMeshPos.x <= metaCornerHigh.x+1; metaMeshPos.x++) {
+			for (metaMeshPos.y = metaCornerLow.y; metaMeshPos.y <= metaCornerHigh.y+1; metaMeshPos.y++) {
+				for (metaMeshPos.z = metaCornerLow.z; metaMeshPos.z <= metaCornerHigh.z+1; metaMeshPos.z++) {
+					RefreshMetaMesh(model, metaMeshPos);
+				}
+			}
+		}
+		
 	}
 	
 	
 	
-	public void RefreshMetaMesh( IVoxModel model, Vector3Int metaMeshPos ) {
+	private byte CountVoxels( IVoxModel model, Vector3Int metaVoxelPos ) {
+		
+		byte voxelCount = 0;
+
+		Vector3Int voxelBasePos = metaVoxelPos * metaVoxelSize;
+		Vector3Int offset = Vector3Int.zero;
+		for ( offset.x = 0; offset.x < metaVoxelSize; offset.x++ ) {
+			for (offset.y = 0; offset.y < metaVoxelSize; offset.y++) {
+				for (offset.z = 0; offset.z < metaVoxelSize; offset.z++) {
+					if ( model.Get( voxelBasePos + offset ) != 0 ) {
+						voxelCount++;
+					}
+				}
+			}
+		}
+		
+		return voxelCount;
+	}
+	
+	
+	
+	private void RefreshMetaMesh( IVoxModel model, Vector3Int metaMeshPos ) {
 		
 		// compute the pattern index
 		byte patternIndex = 0;
-
 		Vector3Int indexOffset = Vector3Int.zero;
-		for ( indexOffset.x=0; indexOffset.x<=1; indexOffset.x++ ) {
+		for ( indexOffset.x=0; indexOffset.x <= 1; indexOffset.x++ ) {
 			for (indexOffset.y = 0; indexOffset.y <= 1; indexOffset.y++) {
 				for (indexOffset.z = 0; indexOffset.z <= 1; indexOffset.z++) {
-					
-					if ( !metaVoxels.AreIndexesValid(metaMeshPos + indexOffset) || metaVoxels[metaMeshPos + indexOffset] == 0 ) {
+
+					Vector3Int metaVoxelPos = metaMeshPos + indexOffset - Vector3Int.one;
+					if ( !metaVoxels.AreIndexesValid(metaVoxelPos) || metaVoxels[metaVoxelPos] == 0 ) {
 						int currentFlag = 1 << (indexOffset.x ^ indexOffset.y + 2 * indexOffset.y + 4 * indexOffset.z);
 						patternIndex |= (byte)currentFlag;
 					}
 				}
 			}
 		}
+
 		
 		
 		// update vertices
 		byte[] currentTriangles = Utils.MarchingCubeTables.TriTable[patternIndex];
-		
-		// Debug.Log("case : " + patternIndex);
-		
-		Vector3 metaVoxelObjectPosition = model.VoxelToObjectPosition(metaMeshPos * metaVoxelSize);
-		foreach ( byte vertexId in currentTriangles ) {
-			VertexLayout currentVertex = new VertexLayout() {
-				position = Utils.MarchingCubeTables.VertexOffsets[vertexId] * metaVoxelSize * model.VoxelSize + metaVoxelObjectPosition,
-				normal = Vector3.up,
-				uv = Vector2.zero
-			};
-			
-			triangles.Add( vertices.Count );
-			vertices.Add( currentVertex );
+		Dictionary<byte, int> vertexLocalToGlobal = RefreshVertices(model, currentTriangles, metaMeshPos);
+
+
+
+
+
+		// update triangles
+		if ( patternIndex == metaMeshToTriangles[metaMeshPos].pattern ) {
+			return;
+		}
+
+		foreach ( int triId in metaMeshToTriangles[metaMeshPos].trianglesIds ) {
+			RemoveTriangle(triId);
+		}
+
+		int[] currentTrianglesGlobal = Array.ConvertAll(currentTriangles, i => vertexLocalToGlobal[i]);
+		for( int i=0; i<currentTrianglesGlobal.Length; i+=3 ) {
+			AddTriangle( new int[]{currentTrianglesGlobal[i], currentTrianglesGlobal[i+1], currentTrianglesGlobal[i+2]} );
 		}
 		
 		
-		// update mesh
-		
-		
+		List<int> trianglesIds = new List<int>();
+		for ( int i=0; i<currentTriangles.Length; i+=3 ) {
+			trianglesIds.Add(triangles.Count-currentTriangles.Length + i );
+		}
+		metaMeshToTriangles[metaMeshPos] = new MetaMesh() { pattern = patternIndex, trianglesIds = trianglesIds };
+
 	}
 	
 	
+	Dictionary<byte, int> RefreshVertices(IVoxModel model, byte[] localIndexes, Vector3Int metaMeshPos) {
+
+		Dictionary<byte, int> vertexLocalToGlobal = new Dictionary<byte, int>();
+		
+		for ( byte i=0; i<12; i++ ) {
+			var (edgeDir, metaEdgePos) = GetVertexLocation(metaMeshPos, i);
+
+			if ( !Array.Exists(localIndexes, x => x == i) ) {
+				RemoveVertex(metaEdgePos, edgeDir);
+			}
+		}
+
+		for ( byte i=0; i<12; i++ ) {
+			var (edgeDir, metaEdgePos) = GetVertexLocation(metaMeshPos, i);
+
+			if (Array.Exists(localIndexes, x => x == i)) {
+				vertexLocalToGlobal[i] = AddVertex(model, metaEdgePos, edgeDir);
+			}
+		}
+
+		return vertexLocalToGlobal;
+	}
+
+	private static (int edgeDir, Vector3Int metaEdgePos) GetVertexLocation(Vector3Int metaMeshPos, byte i) {
+		Vector3 localOffset = Utils.MarchingCubeTables.VertexOffsets[i];
+		int edgeDir = 0;
+		for (int j = 0; j < 3; j++) {
+			if (Mathf.RoundToInt(localOffset[j] * 2) == 1) {
+				edgeDir = j;
+			}
+		}
+
+		Vector3Int metaEdgePos = metaMeshPos;
+		for (int j = 0; j < 3; j++) {
+			if (Mathf.RoundToInt(localOffset[j]) == 1) {
+				metaEdgePos[j]++;
+			}
+		}
+
+		return (edgeDir, metaEdgePos);
+	}
+
+
+	int AddVertex( IVoxModel model, Vector3Int metaPos, int edgeDir ) {
+		
+		if ( vertexIndexes[edgeDir][metaPos] == -1 ) {
+
+			vertexIndexes[edgeDir][metaPos] = vertices.Count;
+			vertices.Add( new VertexLayout() );
+			verticesToTriangles.Add(new VertexBuildData(edgeDir, metaPos));
+		}
+		
+		int vertexId = vertexIndexes[edgeDir][metaPos];
+		
+		Vector3 offset = Vector3.zero;
+		offset[edgeDir] = 0.5f;
+		
+		Vector3 vertexPosition = model.VoxelToObjectPosition(metaPos * metaVoxelSize) + offset * metaVoxelSize * model.VoxelSize;
+		VertexLayout vertex = new VertexLayout() { position = vertexPosition, normal = Vector3.up, uv=Vector2.zero};
+		vertices[vertexId] = vertex;
+		
+		return vertexId;
+	}
+
+
+	private void RemoveVertex(Vector3Int metaMeshPos, int edgeDir) {
+		
+		
+		int vertexId = vertexIndexes[edgeDir][metaMeshPos];
+		
+		if (vertexId == -1) {
+			return;
+		}
+		
+		vertices[vertexId] = vertices[^1];
+		verticesToTriangles[vertexId] = verticesToTriangles[^1];
+		vertexIndexes[verticesToTriangles[vertexId].edgeDir][verticesToTriangles[vertexId].metaEdgePos] = vertexId;
+
+		
+		foreach ( int triId in verticesToTriangles[vertexId].triangleIds ) {
+			
+			for ( int i=triId; i<triId+3; i++ ) {
+				if (triangles[i] == vertices.Count-1 ) {
+					triangles[i] = vertexId;
+				}
+			}
+		}
+		
+		vertexIndexes[edgeDir][metaMeshPos] = -1;
+		vertices.RemoveAt(vertices.Count-1);
+		verticesToTriangles.RemoveAt(verticesToTriangles.Count-1);
+	}
+	
+	
+	
+	private void AddTriangle( int[] verticesIds ) {
+		
+		triangles.AddRange( verticesIds );
+		
+
+		int triId = triangles.Count - 3;
+		foreach ( int vertexId in verticesIds ) {
+			verticesToTriangles[vertexId].triangleIds.Add(triId);
+		}
+	}
+	
+	private void RemoveTriangle( int triIndex ) {
+
+		for ( int i=0; i<3; i++ ) {
+			verticesToTriangles[triangles[triIndex + i]].triangleIds.Remove(triIndex);
+			triangles[triIndex + i] = triangles[triangles.Count - 3 + i];
+
+			int tmp = verticesToTriangles[triangles[triIndex + i]].triangleIds.IndexOf(triangles.Count - 3);
+			verticesToTriangles[triangles[triIndex + i]].triangleIds[tmp] = triIndex;
+		}
+		
+		triangles.RemoveRange(triangles.Count-3, 3);
+	}
+	
+
 }
 }
--- Assets/DestructionSystem/Utils/Serializable3DArray.cs
+++ Assets/DestructionSystem/Utils/Serializable3DArray.cs
@@ -2,29 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace DestructionSystem.Utils
 {
 	[Serializable]
-	public class Serializable3DByteArray
+	public abstract class Serializable3DArray<T>
 	{
-
-		[SerializeField] private List<byte> data;
+		protected abstract List<T> Data {
+			get; 
+			set;
+		}
 
 		[SerializeField] private Vector3Int dim;
 		public Vector3Int Dim => dim;
-
-
-		public Serializable3DByteArray(int dimX, int dimY, int dimZ) :
-			this(new Vector3Int(dimX, dimY, dimZ))
-		{
-		}
-
-
-		public Serializable3DByteArray(Vector3Int dim)
+		
+		
+		
+		
+		public Serializable3DArray(Vector3Int dim, T defaultValue)
 		{
 			this.dim = dim;
-			data = Enumerable.Repeat((byte)0, dim.x * dim.y * dim.z).ToList();
+			Data = Enumerable.Repeat(defaultValue, dim.x * dim.y * dim.z).ToList();
 		}
 
 
@@ -39,16 +38,41 @@ namespace DestructionSystem.Utils
 		}
 		
 		
-		public byte this[Vector3Int indexes] {
-			get { return this[indexes.x, indexes.y, indexes.z]; }
-			set { this[indexes.x, indexes.y, indexes.z] = value; }
+		public T this[Vector3Int indexes] {
+			get => this[indexes.x, indexes.y, indexes.z];
+			set => this[indexes.x, indexes.y, indexes.z] = value;
 		}
 
-		public byte this[int x, int y, int z]
+		public T this[int x, int y, int z]
 		{
-			get { return data[x + y * dim.x + z * dim.x * dim.y]; }
-			set { data[x + y * dim.x + z * dim.x * dim.y] = value; }
+			get => Data[x + y * dim.x + z * dim.x * dim.y];
+			set => Data[x + y * dim.x + z * dim.x * dim.y] = value;
 		}
 	}
+
+
+[Serializable]
+public class Serializable3DByteArray : Serializable3DArray<byte> {
+	
+	[SerializeField] private List<byte> data; 
+	protected override List<byte> Data {
+		get => data;
+		set => data = value;
+	}
+	
+	public Serializable3DByteArray(Vector3Int dim, byte defaultValue=0) : base(dim, defaultValue) {}
+}
+
+[Serializable]
+public class Serializable3DIntArray : Serializable3DArray<int> {
+	
+	[SerializeField] private List<int> data; 
+	protected override List<int> Data {
+		get => data;
+		set => data = value;
+	}
+	public Serializable3DIntArray(Vector3Int dim, int defaultValue=0) : base(dim, defaultValue) {}
+}
+
 
 }
